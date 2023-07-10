f467454a12a129dd4f2756bb5d0e9ffb3413d6af
Anton240896 <antalipov24081111111@gmail.com>
2023-07-10 15:36:11 +0300

    add

--- js.js
+++ js.js
@@ -63,3 +63,43 @@
 // }
 // console.log(palindrom('4340'));
 
+// // 4)
+// const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+// // Change code below this line
+// const firstTwoEls = fruits.slice(0, maxLength) ;
+// console.log(firstTwoEls);
+
+
+// // 5)
+
+// const start = 1;
+// const end = 10;
+
+// for (let i = start; i <=end ; i +=1 ) { // Change this line
+//   console.log(i);
+// }
+
+//  6) Напиши функцію calculateTotal(number), яка приймає ціле число (параметр number) 
+// і повертає суму всіх цілих чисел від одиниці і до цього числа.
+//  Наприклад, якщо number дорівнює 3, то сума - це 1 + 2 + 3, тобто 6.
+
+// Оголошена функція calculateTotal(number)
+// Виклик функції calculateTotal(1) повертає 1
+// Виклик функції calculateTotal(3) повертає 6
+// Виклик функції calculateTotal(7) повертає 28
+// Виклик функції calculateTotal(18) повертає 171
+// Виклик функції calculateTotal(24) повертає 300
+// Виклик функції calculateTotal() з випадковим числом повертає правильне значення
+
+function calculateTotal(number) {
+    let sum = 0;
+  
+    for(let i = 0; i <= number; i+=1){
+      sum += i;
+    }
+  
+    return sum;
+  }
+  
+  console.log(calculateTotal(4))
